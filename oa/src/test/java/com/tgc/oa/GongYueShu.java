ddddfe8dfe28131d8a3a6bc3d46f3bc2c8c0eec7
tgc1005 <453976321@qq.com>
2019-08-12 18:18:27 +0800

    add codes

--- oa/src/test/java/com/tgc/oa/GongYueShu.java
+++ oa/src/test/java/com/tgc/oa/GongYueShu.java
@@ -5,4 +5,20 @@ package com.tgc.oa;
  * @date: 2019/8/12
  */
 public class GongYueShu {
+    public static void main(String[] args) {
+        System.out.println(print(4,8));
+    }
+
+    public static String print(int a, int b){
+        if( a == b){
+            System.out.println("1");
+        }
+
+        for (int i = a; i >=2 ; i--) {
+            if(a % i == 0 && b % i ==0){
+                return a/i+" / "+ b/i;
+            }
+        }
+        return a+" / "+b;
+    }
 }
